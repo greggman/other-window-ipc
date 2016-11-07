c3ec466c80cf87296e2c5dc159dab1102bad01e3
Gregg Tavares <github@greggman.com>
2016-11-07 23:40:05 +0900

    make example show creating channel and streams in main process

--- example/main.js
+++ example/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const electron = require('electron');
-require('../other-window-ipc')
+const otherWindowIPC = require('../other-window-ipc')
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -17,13 +17,61 @@ function createWindow (off, url) {
   return window;
 }
 
+// Example of also creating a channel in the main process
+const channel = otherWindowIPC.createChannel("pirateTalk");
+channel.on('connect', stream => {
+
+  stream.on('say', (msg, name) => {
+    console.log("got say:", msg, name);
+    stream.send('ptalk', "Yo " + name + ". You said " + msg + ", arrrgh!");
+  });
+
+  stream.on('disconnect', () => {
+    console.log("disconnected");
+  });
+
+});
+
+
+
 let commonWindow;
 let test1Window;
 let test2Window;
 
 app.on('ready', () => {
   commonWindow = createWindow(100, `file://${__dirname}/common-window.html`);
-  test1Window = createWindow(200, `file://${__dirname}/test-window.html?Jack`);
-  test2Window = createWindow(300, `file://${__dirname}/test-window.html?Jill`);
+  // give commonWindow a chance to open. Seems like it's possible for test window to start
+  // before there's a channel to talk to
+  setTimeout(() => {
+    test1Window = createWindow(200, `file://${__dirname}/test-window.html?Jack`);
+    test2Window = createWindow(300, `file://${__dirname}/test-window.html?Jill`);
+  }, 500);
+
+  // Example of talking to the "blarg" channel in common-window
+  setTimeout(() => {
+
+    var once = true;
+    const name = "the-browser-main-process";
+    otherWindowIPC.createChannelStream("blarg")
+    .then(stream => {
+
+      stream.on('hello', (...args) => {
+        console.log("got msg from other:", ...args);
+        if (once) {
+          once = false;
+          stream.send('hello', 'hello again from ' + name);
+        } else {
+          console.log("close");
+          stream.close();
+        }
+      });
+      stream.send('hello', 'hello from ' + name);
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  }, 1000);  // I hope this is enough time for common-window to open
 });
 
--- example/test-window.js
+++ example/test-window.js
@@ -5,6 +5,7 @@ const log = require('./log');
 var once = true;
 var name = window.location.search.substring(1);
 
+// Talk to common-window's "blarg" channel
 otherWindowIPC.createChannelStream("blarg")
 .then(stream => {
 
@@ -19,6 +20,27 @@ otherWindowIPC.createChannelStream("blarg")
     }
   });
   stream.send('hello', 'hello from ' + name);
+
+})
+.catch(err => {
+  console.log(err);
+});
+
+// Talk to browser (main) process "pirateTalk" channel
+otherWindowIPC.createChannelStream("pirateTalk")
+.then(stream => {
+
+  stream.on('ptalk', (...args) => {
+    log("pirate saidr:", ...args);
+    if (once) {
+      once = false;
+      stream.send('say', 'Arg to you, from', name);
+    } else {
+      log("close");
+      stream.close();
+    }
+  });
+  stream.send('say', "Hi Matey! It's ", name);
 
 })
 .catch(err => {
